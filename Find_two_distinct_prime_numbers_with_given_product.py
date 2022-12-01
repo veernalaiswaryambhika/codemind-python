f4e5fac7cd52642681993af8ee8918c2cf2d8157
veernalaiswaryambhika <84122740+veernalaiswaryambhika@users.noreply.github.com>
2022-12-01 19:21:15 +0530

    import math def fun(n):     if n==0:         return False     for i in range(2,int(math.sqrt(n))+1):         if n%i==0:             return False     return True n=int(input()) l=[] t1=0 t2=0 for i in range(1,n):     if fun(i):         l.append(i) for i in range(len(l)):     for j in range(i+1,len(l)):         if l[i]*l[j]==n:             t1=l[i]             t2=l[j] if t1==0 and t2==0:     print(-1) else:     print(t1,t2)

---
+++ Find_two_distinct_prime_numbers_with_given_product.py
@@ -0,0 +1,24 @@
+import math
+def fun(n):
+    if n==0:
+        return False
+    for i in range(2,int(math.sqrt(n))+1):
+        if n%i==0:
+            return False
+    return True
+n=int(input())
+l=[]
+t1=0
+t2=0
+for i in range(1,n):
+    if fun(i):
+        l.append(i)
+for i in range(len(l)):
+    for j in range(i+1,len(l)):
+        if l[i]*l[j]==n:
+            t1=l[i]
+            t2=l[j]
+if t1==0 and t2==0:
+    print(-1)
+else:
+    print(t1,t2)
