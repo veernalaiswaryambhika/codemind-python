1187f11b14d257f7fcade4b8f25b38b96ecb27af
veernalaiswaryambhika <84122740+veernalaiswaryambhika@users.noreply.github.com>
2022-12-01 20:02:10 +0530

    n=int(input()) a=0 b=1 c=0 l=[0,1] l1=[] l2=[] for i in range(1,n+1):     c=a+b     a=b     b=c     l.append(c) for i in range(len(l)):     if l[i]<n:         l1.append(l[i])     else:         l2.append(l[i]) if n in l:     l.remove(n) x=max(l1) y=min(l2) d1=n-max(l1) d2=min(l2)-(n) if(d1==d2):     print(x,y) elif(d1<d2):     print(x) else:     print(y)

---
+++ nearest_fibonacci.py
@@ -0,0 +1,29 @@
+n=int(input())
+a=0
+b=1
+c=0
+l=[0,1]
+l1=[]
+l2=[]
+for i in range(1,n+1):
+    c=a+b
+    a=b
+    b=c
+    l.append(c)
+for i in range(len(l)):
+    if l[i]<n:
+        l1.append(l[i])
+    else:
+        l2.append(l[i])
+if n in l:
+    l.remove(n)
+x=max(l1)
+y=min(l2)
+d1=n-max(l1)
+d2=min(l2)-(n)
+if(d1==d2):
+    print(x,y)
+elif(d1<d2):
+    print(x)
+else:
+    print(y)
